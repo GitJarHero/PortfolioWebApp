1cb73460fddb6cd943bd70171ec48441804dd6c0
khkalli <p.kahler@deindesign.com>
2025-05-12 23:15:31 +0200

    added important todo & moved Sending of global message to the service layer

--- PortfolioWebApp/Services/Chat/GlobalChatService.cs
+++ PortfolioWebApp/Services/Chat/GlobalChatService.cs
@@ -1,19 +1,24 @@
+using Microsoft.AspNetCore.SignalR;
 using PortfolioWebApp.Models;
 using PortfolioWebApp.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using PortfolioWebApp.Hubs;
 
 namespace PortfolioWebApp.Services;
 
 public class GlobalChatService {
     
     private readonly AppDbContext _dbContext;
+    private readonly IHubContext<GlobalChatHub> _globalChatHubContext;
 
-    public GlobalChatService(AppDbContext dbContext) {
+    public GlobalChatService(AppDbContext dbContext, IHubContext<GlobalChatHub> hubContext) {
         _dbContext = dbContext;
+        _globalChatHubContext = hubContext;
     }
 
     public async Task SendMessage(GlobalMessage message) {
         await SaveMessage(message);
+        await _globalChatHubContext.Clients.All.SendAsync("ReceiveMessage", message.User.UserName, message.Content);
     }
 
     public async Task<List<GlobalMessage>> LoadLatestMessages(int n) {
